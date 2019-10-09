a3e3a30ecd5b455ad965aa5b587b4040b4da9be0
nataliacastro1 <56360826+nataliacastro1@users.noreply.github.com>
2019-10-09 13:29:30 -0500

    version 1.0
    creacion de 5 atributos de clase

---
+++ src/casas/Casas.java
@@ -0,0 +1,16 @@
+package casas;
+
+
+import java.util.Date;
+
+
+public class Casas {
+ private Date fechadeconstruccion;
+ private boolean diferenteszonas;
+ private String nombredelaconstructora;
+ private int numerodeviviendas;
+ private Date fechadefinalizacion;
+ 
+ 
+    
+}
